33f06c4ffe87f6c9c7e98c3a0f6fcef5942ffb22
Marcela Prata Silvério <marcelapsilverio@gmail.com>
2021-12-17 16:47:18 -0300

    Update DataConfiguration.java

--- src/main/java/com/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -18,7 +18,7 @@ public class DataConfiguration {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useTimezone=true&serverTimezone=UTC");
         dataSource.setUsername("root");
-        dataSource.setPassword("marcela891260");
+        dataSource.setPassword("root");
         return dataSource;
     }
 
