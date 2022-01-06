7b5adb7e0713fcb0c6d44c81790e6d9b195c611b
Marcela Silverio <marcelapsilverio@gmail.com>
2022-01-06 11:01:40 -0300

    Lesson 9

--- src/main/java/com/eventoapp/models/Evento.java
+++ src/main/java/com/eventoapp/models/Evento.java
@@ -1,10 +1,14 @@
 package com.eventoapp.models;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.io.Serializable;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Evento implements Serializable {
@@ -19,6 +23,9 @@ public class Evento implements Serializable {
     private String local;
     private String data;
     private String horario;
+
+    @OneToMany
+    private List<Convidado> convidados;
 
     public long getCodigo() {
         return codigo;
